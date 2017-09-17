c85b3cf62f88ab8e5ba91f50f404061a2a5db4b5
viper1010 <viper.lannister@gmail.com>
2017-09-17 18:46:18 -0400

    Add connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 require('./config/config');
-const express = require('express');
 const path = require('path');
+const http = require('http');
+const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 console.log('Public Path - ', publicPath);
 
 let app = express();
 let port = process.env.PORT;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
+io.on('connection', (socket)=>{
+  console.log('New User Connected');
 
+  socket.on('disconnect', ()=>{
+    console.log('User is disconnected.')
+  })
+});
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
   console.log('Chat App started at port - ', port);
 });
