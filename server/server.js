1d93526f1906e732482037d618fbab321707a6f2
viper1010 <viper.lannister@gmail.com>
2017-09-22 19:34:54 -0400

    Added test cases and refactored MEssage

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message')
+
 const publicPath = path.join(__dirname, '../public');
 console.log('Public Path - ', publicPath);
 
@@ -20,27 +22,14 @@ io.on('connection', (socket)=>{
   console.log('New User Connected');
 
   // Send a message to "this socket" - the one that connected
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the Chat App',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined.',
-    createdAt: new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
   socket.on('createMessage', (message)=>{
-    console.log('New message received - ', message);
 
     // SENDS to Everyone connected
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
     // Send to everyone except "this socket"
     // socket.broadcast.emit('newMessage', {
